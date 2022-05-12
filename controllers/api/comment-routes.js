cb5c4fcc6925c750d634140b19b9cbf7c1ea9b17
Becca Yu <ugsung8970@gmail.com>
2022-05-11 21:14:34 -0700

    testing5

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
@@ -11,13 +12,24 @@ router.get('/', (req, res) => {
       });
 });
 
+router.get('/:id', (req, res) => {
+  Comment.findAll({
+          where: {
+              id: req.params.id
+          }
+      })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      })
+});
+
 router.post('/', withAuth, (req, res) => {
-  // check the session
   if (req.session) {
     Comment.create({
       comment_text: req.body.comment_text,
       post_id: req.body.post_id,
-      // use the id from the session
       user_id: req.session.user_id,
     })
       .then(dbCommentData => res.json(dbCommentData))
